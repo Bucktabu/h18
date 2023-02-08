6f56265cb0b53cb83e5d377d9ddf26d8bf437872
Stanislav <stanislaworonin@gmail.com>
2023-02-08 22:08:45 +0300

    some fix

--- src/modules/blogger/api/blogger-users.controller.ts
+++ src/modules/blogger/api/blogger-users.controller.ts
@@ -48,6 +48,7 @@ export class BloggerUsersController {
     @Body() dto: BanUserDto,
     @Param('id') userId: string,
   ) {
+    console.log(dto, 'banUserDTO');
     const result = await this.blogsService.updateUserBanStatus(userId, dto);
 
     if (!result) {
